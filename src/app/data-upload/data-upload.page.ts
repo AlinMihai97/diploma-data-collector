0e3310aff8714e447f15c99374175eb9679d5cc2
unknown <alinmihai97@gmail.com>
2019-04-09 21:26:03 +0300

    upload page

--- src/app/data-upload/data-upload.page.ts
+++ src/app/data-upload/data-upload.page.ts
@@ -7,9 +7,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataUploadPage implements OnInit {
 
-  constructor() { }
+  itemList = [
+    {
+      loadingText: "Checking User",
+      succesText: "User Registered",
+      displayClass: "",
+      work: function() {
+
+      },
+      isLoading: true,
+      isSucces: false
+    },
+    {
+      loadingText: "Checking User",
+      succesText: "User Registered",
+      displayClass: "",
+      isLoading: true,
+      isSucces: false
+    },
+    {
+      loadingText: "Checking User",
+      succesText: "User Registered",
+      displayClass: "",
+      isLoading: true,
+      isSucces: false
+    },
+    {
+      loadingText: "Checking User",
+      succesText: "User Registered",
+      displayClass: "",
+      isLoading: true,
+      isSucces: false
+    }
+  ]
+  hideClassName = "hideItem"
+  showClassName = "fadeIn"
+  constructor() {
+    this.itemList.forEach(item =>
+    {
+      item.displayClass = this.hideClassName;
+    });
+    this.doProcess(0);
+   }
 
   ngOnInit() {
   }
 
+  doProcess(itemIndex) {
+    if(itemIndex < this.itemList.length) {
+      let currentItem = this.itemList[itemIndex];
+      currentItem.displayClass = this.showClassName;
+      setTimeout(() => {
+        currentItem.isLoading = false;
+        currentItem.isSucces = true;
+        this.doProcess(itemIndex+1);
+      }, 2000);
+    }
+  }
 }
